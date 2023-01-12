44bac05833bde9515ffb3572d05a8ba6b587bd04
Martín Fiori <martinfiori@outlook.com>
2023-01-12 07:44:55 -0300

    checking apikey and md5 hash

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const API_URL =
+		"https://gateway.marvel.com/v1/public/characters?apikey=ed5aa221d74a4d0812e9637da4fd9ff2&hash=3cdcd0023e2fbb15efaad3438a70be77";
+	const [arr, setArr] = React.useState([]);
+	const apiCall = async () => {
+		try {
+			const fetch = await axios(API_URL);
+			const {
+				data: { data },
+			} = fetch;
+			console.log(data.results);
+			setArr(data.results);
+		} catch (err) {
+			console.error(err.message);
+		}
+	};
+	React.useEffect(() => {
+		apiCall();
+	}, []);
+	return (
+		<div className="App">
+			{arr.map((el, i) => (
+				<div key={i}>
+					<h1>{el.name}</h1>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default App;
