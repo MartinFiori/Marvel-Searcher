64c48f128cbdf1e91c27720926bb5d7af2adfdae
Martín Fiori <martinfiori@outlook.com>
2023-01-18 20:26:04 -0300

    adding ThemeProvider

--- src/Views/HomeView.jsx
+++ src/Views/HomeView.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import CardsContainer from "../Components/CardsContainer/CardsContainer";
+import { CharactersContext } from "../Context/CharactersContext";
 
 export default function HomeView() {
-	return <CardsContainer />;
+	const { userSearch, randomImg } = useContext(CharactersContext);
+
+	return userSearch ? (
+		<CardsContainer />
+	) : (
+		<img
+			src={randomImg}
+			style={{
+				maxWidth: "500px",
+				display: "block",
+				marginInline: "auto",
+				marginTop: "3rem",
+				borderRadius: "5px",
+			}}
+			alt="Home"
+		/>
+	);
 }
